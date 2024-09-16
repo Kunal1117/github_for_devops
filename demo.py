680cf10ade1931019c3fa46d50973bdf474f4a02
unknown <kunaladhangle124@gmail.com>
2024-09-16 21:15:40 +0530

    Added demo file

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
-def my_function():
-    a = 5
-    b = 6
+#def my_function():
+#    a = 5
+#    b = 6
 
-    return d
+#    return d
 
-my_function()
+#my_function()
--- demo2.py
+++ demo2.py
@@ -1,7 +1,7 @@
-def my_function():
-    a = 5
-    b = 6
+#def my_function():
+#    a = 5
+#    b = 6
 
-    return 6
+#   return 6
 
-my_function()
+#my_function()
